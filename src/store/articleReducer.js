38d4cc23910266b6d3d7ed9cf88e00bb1da22c7c
Megha Sahgal <meghas@gmail.com>
2022-10-13 04:55:59 +0000

    Update articleReducer.js

--- src/store/articleReducer.js
+++ src/store/articleReducer.js
@@ -14,7 +14,12 @@ const initialState = { entries: [], isLoading: true };
 const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
-      return { ...state, entries: [...action.articles] };
+    // ...state is a copy of the state
+    // const newState = {...state};
+    // newState.entries = [...state.entries]// everything that was in the old state will be in the array + add the actions
+    // return newState;
+      return { ...state, entries: [...state.entries, ...action.articles] };
+    //   spread action.articles in the entries
     default:
       return state;
   }
